94ed988e2352d29d5492c29e0883abaf5308a30e
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-03-15 16:16:39 +0545

    'FormattedAndFinishing'

--- app/src/components/forecast/Forecast.jsx
+++ app/src/components/forecast/Forecast.jsx
@@ -4,29 +4,51 @@ import {
   AccordionItem,
   AccordionItemButton,
   AccordionItemPanel,
-  AccordionItemHeading
+  AccordionItemHeading,
 } from "react-accessible-accordion";
 
-const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+const week = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 const Forecast = ({ data }) => {
   const days = new Date().getDay();
-const forecastDays =  week.slice(days, week.length).concat(week.slice(0, days))
-console.log(forecastDays)
+  const forecastDays = week
+    .slice(days, week.length)
+    .concat(week.slice(0, days));
+  console.log(forecastDays);
   return (
     <>
-      <label className="text-2xl font-bold text-left">Forecast for next 7 days</label>
+      <label className="text-2xl font-bold text-left">
+        Forecast for next 7 days
+      </label>
       <Accordion allowZeroExpanded>
         {data.list.splice(0, 7).map((item, idx) => (
           <AccordionItem key={idx}>
             <AccordionItemHeading>
               <AccordionItemButton>
                 <div className="rounded-2xl h-10 m-1.5 shadow-md items-center flex cursor-pointer text-sm pt-1 pb-1 pl-5 pr-5">
-                  <img className="w-10" alt="weather" src={`images/${item.weather[0].icon}.png`}></img>
-                  <label className="flex-1 font-semibold ml-3.5 text-left">{forecastDays[idx]}</label>
-                  <label className="flex-1 mr-3.5 text-right">{item.weather[0].description}</label>
-                  <label className="text-gray-500">{Math.round(item.main.temp_min - 273.15)}°C / {Math.round(item.main.temp_max - 273.15)}°C</label>
-
+                  <img
+                    className="w-10"
+                    alt="weather"
+                    src={`images/${item.weather[0].icon}.png`}
+                  ></img>
+                  <label className="flex-1 font-semibold ml-3.5 text-left">
+                    {forecastDays[idx]}
+                  </label>
+                  <label className="flex-1 mr-3.5 text-right">
+                    {item.weather[0].description}
+                  </label>
+                  <label className="text-gray-500">
+                    {Math.round(item.main.temp_min - 273.15)}°C /{" "}
+                    {Math.round(item.main.temp_max - 273.15)}°C
+                  </label>
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
