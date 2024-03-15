81e36def863c96ccd43c6943108798f132b23da8
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-03-15 15:29:42 +0545

    'withoutCSS'

--- app/src/components/forecast/Forecast.jsx
+++ app/src/components/forecast/Forecast.jsx
@@ -7,10 +7,15 @@ import {
   AccordionItemHeading
 } from "react-accessible-accordion";
 
+const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
 const Forecast = ({ data }) => {
+  const days = new Date().getDay();
+const forecastDays =  week.slice(days, week.length).concat(week.slice(0, days))
+console.log(forecastDays)
   return (
     <>
-      <label>Daily</label>
+      <label className="text-2xl font-bold">Forecast for next 7 days</label>
       <Accordion allowZeroExpanded>
         {data.list.splice(0, 7).map((item, idx) => (
           <AccordionItem key={idx}>
@@ -18,6 +23,10 @@ const Forecast = ({ data }) => {
               <AccordionItemButton>
                 <div>
                   <img alt="weather" src={`images/${item.weather[0].icon}.png`}></img>
+                  <label className="">{forecastDays[idx]}</label>
+                  <label>{item.weather[0].description}</label>
+                  <label>{Math.round(item.main.temp_min - 273.15)}°C / {Math.round(item.main.temp_max - 273.15)}°C</label>
+
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
