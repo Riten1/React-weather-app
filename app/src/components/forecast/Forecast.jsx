8c312f00517f8ca1e4427f8a905e1e71947a633a
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-03-15 16:14:55 +0545

    'CSSFinished'

--- app/src/components/forecast/Forecast.jsx
+++ app/src/components/forecast/Forecast.jsx
@@ -15,7 +15,7 @@ const forecastDays =  week.slice(days, week.length).concat(week.slice(0, days))
 console.log(forecastDays)
   return (
     <>
-      <label className="text-2xl font-bold">Forecast for next 7 days</label>
+      <label className="text-2xl font-bold text-left">Forecast for next 7 days</label>
       <Accordion allowZeroExpanded>
         {data.list.splice(0, 7).map((item, idx) => (
           <AccordionItem key={idx}>
@@ -23,7 +23,7 @@ console.log(forecastDays)
               <AccordionItemButton>
                 <div className="rounded-2xl h-10 m-1.5 shadow-md items-center flex cursor-pointer text-sm pt-1 pb-1 pl-5 pr-5">
                   <img className="w-10" alt="weather" src={`images/${item.weather[0].icon}.png`}></img>
-                  <label className="flex-1 font-semibold ml-3.5">{forecastDays[idx]}</label>
+                  <label className="flex-1 font-semibold ml-3.5 text-left">{forecastDays[idx]}</label>
                   <label className="flex-1 mr-3.5 text-right">{item.weather[0].description}</label>
                   <label className="text-gray-500">{Math.round(item.main.temp_min - 273.15)}°C / {Math.round(item.main.temp_max - 273.15)}°C</label>
 
@@ -31,34 +31,34 @@ console.log(forecastDays)
               </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
-              <div>
-                <div>
-                  <label>Pressure: </label>
+              <div className="text-xs flex justify-between">
+                <div className="text-right ">
+                  <label className="text-gray-500">Pressure: </label>
                   <label>{item.main.pressure} hPa</label>
                 </div>
 
-                <div>
-                  <label>Humidity: </label>
+                <div className="text-right">
+                  <label className="text-gray-500">Humidity: </label>
                   <label>{item.main.humidity} %</label>
                 </div>
 
-                <div>
-                  <label>Clouds: </label>
+                <div className="text-right">
+                  <label className="text-gray-500">Clouds: </label>
                   <label>{item.clouds.all} %</label>
                 </div>
 
-                <div>
-                  <label>Speed: </label>
+                <div className="text-right">
+                  <label className="text-gray-500">Speed: </label>
                   <label>{item.wind.speed} m/s</label>
                 </div>
 
-                <div>
-                  <label>Sea level: </label>
+                <div className="text-right">
+                  <label className="text-gray-500">Sea level: </label>
                   <label>{item.main.sea_level} m</label>
                 </div>
 
-                <div>
-                  <label>Feels like: </label>
+                <div className="text-right">
+                  <label className="text-gray-500">Feels like: </label>
                   <label>{Math.round(item.main.feels_like - 273.15)}°C</label>
                 </div>
               </div>
